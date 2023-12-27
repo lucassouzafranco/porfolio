b798846d9f82b706bad82ebe59aed9ff238d5454
Lucas Souza <lucassouzafranco1@gmail.com>
2023-12-27 20:33:27 -0300

    ✨ feat(menu): basic component structure
    hover property & dark mode icon

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const Menu = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '98.4vw',
+        color: 'black',
+        padding: '10px',
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <DarkModeIcon sx={{ marginLeft: '1.5em', fontSize: '1.7em' }} />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography 
+          sx={{ 
+            margin: '0 0.7em', 
+            fontSize: '1em', 
+            '&:hover': { color: '#0C8CE9'}, 
+            cursor: 'pointer' 
+          }}
+        >Sobre mim
+        </Typography>
+        <Typography 
+          sx={{ 
+            margin: '0 0.7em', 
+            fontSize: '1em', 
+            '&:hover': { color: '#0C8CE9'}, 
+            cursor: 'pointer' 
+          }}
+        >Habilidades
+        </Typography>
+        <Typography 
+          sx={{ 
+            margin: '0 0.7em', 
+            fontSize: '1em', 
+            '&:hover': { color: '#0C8CE9'}, 
+            cursor: 'pointer' 
+          }}
+        >Experiências
+        </Typography>
+        <Typography 
+          sx={{ 
+            margin: '0 0.7em', 
+            fontSize: '1em', 
+            '&:hover': { color: '#0C8CE9'}, 
+            cursor: 'pointer' 
+          }}
+        >Contato
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Menu;
